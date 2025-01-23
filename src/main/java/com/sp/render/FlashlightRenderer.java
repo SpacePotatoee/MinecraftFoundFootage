7bc133df73bbbe42aa65a8def4cda77e2d60f168
SpacePotato <rubywindowofficial1@gmail.com>
2025-01-23 03:31:22 -0500

    Made the flashlight look more like a flashlight

--- src/main/java/com/sp/render/FlashlightRenderer.java
+++ src/main/java/com/sp/render/FlashlightRenderer.java
@@ -9,57 +9,83 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Quaternionf;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 
 public class FlashlightRenderer {
     private final MinecraftClient client;
-    public final HashMap<AbstractClientPlayerEntity, AreaLight> flashLightList;
+    public final HashMap<AbstractClientPlayerEntity, ArrayList<AreaLight>> flashLightList2;
 
     public FlashlightRenderer(){
         this.client = MinecraftClient.getInstance();
-        this.flashLightList = new HashMap<>();
+        this.flashLightList2 = new HashMap<>();
     }
 
-    public void renderFlashlightForEveryPlayer(float partialTicks){
+    public void renderFlashlightForEveryPlayer(float partialTicks) {
         if(client.world != null) {
             List<AbstractClientPlayerEntity> playerList = client.world.getPlayers();
 
             for (AbstractClientPlayerEntity player : playerList) {
                 if (player != null) {
+                    if(player.isSpectator() && !player.equals(client.player)){
+                        tryToRemoveFlashlight(player);
+                        continue;
+                    }
                     PlayerComponent playerComponent = InitializeComponents.PLAYER.get(player);
                     if (playerComponent.isFlashLightOn()) {
                         Vec3d playerPos = player.getCameraPosVec(partialTicks);
-                        if (!flashLightList.containsKey(player)) {
+                        if (!flashLightList2.containsKey(player)) {
                             AreaLight areaLight = new AreaLight();
+                            AreaLight areaLight2 = new AreaLight();
+                            Quaternionf orientation = new Quaternionf().rotateXYZ((float) -Math.toRadians(player.getPitch(partialTicks)), (float) Math.toRadians(player.getYaw(partialTicks)), 0.0f);
                             VeilRenderSystem.renderer().getDeferredRenderer().getLightRenderer().addLight(areaLight
                                     .setBrightness(1f)
                                     .setDistance(25f)
                                     .setSize(0, 0)
                                     .setPosition(playerPos.getX(), playerPos.getY(), playerPos.getZ())
-                                    .setOrientation(new Quaternionf().rotateXYZ((float) -Math.toRadians(player.getPitch(partialTicks)), (float) Math.toRadians(player.getYaw(partialTicks)), 0.0f))
+                                    .setOrientation(orientation)
                             );
-                            flashLightList.put(player, areaLight);
+                            VeilRenderSystem.renderer().getDeferredRenderer().getLightRenderer().addLight(areaLight2
+                                    .setBrightness(1f)
+                                    .setAngle(0.25f)
+                                    .setDistance(25f)
+                                    .setSize(0, 0)
+                                    .setPosition(playerPos.getX(), playerPos.getY(), playerPos.getZ())
+                                    .setOrientation(orientation)
+                            );
+                            ArrayList<AreaLight> list = new ArrayList<>();
+                            list.add(areaLight);
+                            list.add(areaLight2);
+                            flashLightList2.put(player, list);
                         } else {
-                            AreaLight areaLight = flashLightList.get(player);
+                            ArrayList<AreaLight> areaLightList = flashLightList2.get(player);
 
-                            Quaternionf currentRot = new Quaternionf().rotateXYZ((float) -Math.toRadians(player.getPitch(partialTicks)), (float) Math.toRadians(player.getYaw(partialTicks)), 0.0f);
-                            areaLight.getOrientation().slerp(currentRot, 0.02f);
-                            areaLight.setPosition(playerPos.getX(), playerPos.getY(), playerPos.getZ());
+                            for(AreaLight areaLights : areaLightList){
+                                Quaternionf currentRot = new Quaternionf().rotateXYZ((float) -Math.toRadians(player.getPitch(partialTicks)), (float) Math.toRadians(player.getYaw(partialTicks)), 0.0f);
+                                areaLights.getOrientation().slerp(currentRot, 0.02f);
+                                areaLights.setPosition(playerPos.getX(), playerPos.getY(), playerPos.getZ());
+                            }
                         }
                     } else {
-                        if (flashLightList.containsKey(player) && flashLightList.get(player) != null) {
-                            AreaLight areaLight = flashLightList.get(player);
-                            VeilRenderSystem.renderer().getDeferredRenderer().getLightRenderer().removeLight(areaLight);
-                            flashLightList.remove(player);
-                        }
+                        tryToRemoveFlashlight(player);
                     }
                 }
             }
         }
 
 
+    }
+
+    private void tryToRemoveFlashlight(AbstractClientPlayerEntity player){
+        if (flashLightList2.containsKey(player) && flashLightList2.get(player) != null) {
+            ArrayList<AreaLight> areaLightList = flashLightList2.get(player);
+            for(AreaLight areaLights : areaLightList){
+                VeilRenderSystem.renderer().getDeferredRenderer().getLightRenderer().removeLight(areaLights);
+            }
+            flashLightList2.remove(player);
+        }
     }
 
 }
