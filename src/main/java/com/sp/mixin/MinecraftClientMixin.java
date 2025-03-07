455a57dab9c1708133a469f36c92df982627511e
HerrChaos <138036657+HerrChaos@users.noreply.github.com>
2025-03-08 00:35:45 +0100

    final fix for the server dedicated issues.

--- src/main/java/com/sp/mixin/MinecraftClientMixin.java
+++ src/main/java/com/sp/mixin/MinecraftClientMixin.java
@@ -1,17 +1,14 @@
 package com.sp.mixin;
 
 import com.sp.SPBRevamped;
-import com.sp.SPBRevampedClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.Perspective;
 import net.minecraft.resource.ResourcePackManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
